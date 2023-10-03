a32a9d300f32b9f40c3cce1e8f5b863d87d811b7
Khaled Korfali <celine.kor7@gmail.com>
2023-10-02 21:36:21 -0700

    Added new method using monolog dependency

--- profile_page.php
+++ profile_page.php
@@ -1,43 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile Cards</title>
-    <style>
-        <?php
-        // Include the CSS code as-is
-        include 'styles.css';
-        ?>
+<?php
+require_once 'vendor/autoload.php'; // Include Composer's autoloader
 
-        /* Add custom CSS for arranging the profile cards side by side */
-        .profile-container {
-            display: flex;
-            justify-content: space-between;
-            margin: 20px; /* Adjust the margin as needed */
-        }
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
-         /* Add CSS to change the text color of the title to white */
-         h1 {
-            color: white;
-        }
-    </style>
-</head>
-<body>
-    <h1>Profile Page</h1>
-    <div class="profile-container">
-        <?php
-        // Include the HTML code for the first profile card
-        include 'profile.html';
-        include 'profile.html';
-        include 'profile.html';
-        ?>
-    </div>
-    <div class="profile-container">
-        <?php
-        // Include the HTML code for the second profile card
-        include 'profile.html';
-        ?>
-    </div>
-</body>
-</html>
+// Create a Monolog logger instance
+$log = new Logger('my_http_logger');
+$log->pushHandler(new StreamHandler('logs/http.log', Logger::INFO));
+
+// Check if the request URL matches the endpoint you want to log
+if ($_SERVER['REQUEST_URI'] === '/your-endpoint') {
+    // Log a message
+    $log->info('HTTP request received for /your-endpoint');
+    
+    // Add your code to handle the HTTP request
+    // For example, you can send an HTTP response here
+    header('Content-Type: text/plain');
+    echo 'Hello, this is your endpoint!';
+} else {
+    // Include the HTML code for the web page
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Profile Cards</title>
+        <style>
+            <?php
+            // Include the CSS code as-is
+            include 'styles.css';
+            ?>
+
+            /* Add custom CSS for arranging the profile cards side by side */
+            .profile-container {
+                display: flex;
+                justify-content: space-between;
+                margin: 20px; /* Adjust the margin as needed */
+            }
+
+             /* Add CSS to change the text color of the title to white */
+             h1 {
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Profile Page</h1>
+        <div class="profile-container">
+            <?php
+            // Include the HTML code for the first profile card
+            include 'profile.html';
+            include 'profile.html';
+            include 'profile.html';
+            ?>
+        </div>
+        <div class="profile-container">
+            <?php
+            // Include the HTML code for the second profile card
+            include 'profile.html';
+            ?>
+        </div>
+    </body>
+    </html>
+    <?php
+}
+?>
