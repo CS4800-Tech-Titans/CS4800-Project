092e7879350881390b6846c06ea77955e61664d5
Khaled Korfali <celine.kor7@gmail.com>
2023-11-19 22:56:50 -0800

    Join button is disabled if user already in group

--- classes/groups/index.php
+++ classes/groups/index.php
@@ -1,50 +1,29 @@
 <?php
-    include_once "../protected/ensureLoggedIn.php";
-    include "../protected/connSql.php";
+include_once "../protected/ensureLoggedIn.php";
+include "../protected/connSql.php";
 
-    if ($_SESSION["role"] == 0) // student role
-    {
-        $stmt = $conn->prepare("SELECT groups.id, groups.name, groups.description, groups.photo FROM `groups` 
-                                JOIN linkUserClass 
-                                ON linkUserClass.userId = ? 
-                                AND linkUserClass.classId = ? 
-                                WHERE groups.classId = ?;");
+if ($_SESSION["role"] == 0) // student role
+{
+    $stmt = $conn->prepare("SELECT groups.id, groups.name, groups.description, groups.photo, 
+                                  IF(linkUserGroup.userId IS NOT NULL, 1, 0) AS isUserInGroup
+                           FROM `groups`
+                           LEFT JOIN linkUserGroup ON linkUserGroup.userId = ? AND linkUserGroup.groupId = groups.id
+                           JOIN linkUserClass ON linkUserClass.userId = ? AND linkUserClass.classId = ?
+                           WHERE groups.classId = ?;");
 
-        // SQL query here gets the classes that the user is in, the name of the classes, and the name of the teacher. 
-        // Inner subquery gets the list of class IDs assosciated with the user, and the first join joins it with the class table
-        // Second join adds the column for the teacher's name. 
+    $stmt->bind_param("iiii", $_SESSION["userId"], $_SESSION["userId"], $classId, $classId);
 
-        $stmt->bind_param("iii", $_SESSION["userId"], $classId, $classId);
+    $stmt->execute();
 
-        $stmt->execute();
-
-        $stmt->bind_result($groupId, $groupName, $groupDescription, $groupPhoto);
-    }
-    /*else if ($_SESSION["role"] == 1) 
-    {
-        $stmt = $conn->prepare("SELECT classes.id, classes.name FROM `classes` WHERE classes.teacherId = ?;");
-
-        // SQL query here gets the classes that the user is teacher of, since this user is a teacher (role is 1). Nice simple query. 
-
-        $stmt->bind_param("s",  $_SESSION["userId"]);
-
-        $stmt->execute();
-
-        $stmt->bind_result($classId, $className);
-        $teacherName = "";
-    }*/
-    
-    $groupCount = 0;
+    $stmt->bind_result($groupId, $groupName, $groupDescription, $groupPhoto, $isUserInGroup);
+}
+$groupCount = 0;
 ?>
-
-<!--<link rel="stylesheet" type="text/css" href="style.css">-->
 
 <style>
     <?php include "style.css"?>
-</style>
-<style>
-    /* Add custom CSS for the plus button */
-    .add-group-button {
+     /* Add custom CSS for the plus button */
+     .add-group-button {
         position: fixed;
         bottom: 20px;
         right: 20px;
@@ -68,7 +47,6 @@
     }
 </style>
 
-
 <body translate="no">
     <h2 style="color:black;">Groups</h2>
     <ul class="cards">
@@ -81,7 +59,13 @@
                     <div class="card__content">
                         <div class="card__title"><?=$groupName?></div>
                         <p class="card__text"><?=$groupDescription?></p>
-                        <button class="join-group-button" data-group-id="<?=$groupId?>">Join</button>
+                        <?php
+                            if ($isUserInGroup) {
+                                echo '<button class="join-group-button" disabled>Joined</button>';
+                            } else {
+                                echo '<button class="join-group-button" data-group-id="'.$groupId.'">Join</button>';
+                            }
+                        ?>
                     </div>
                 </a>
             </li>
@@ -89,10 +73,11 @@
         <?php if ($groupCount == 0) { ?>
             <p style="color:black">There are no groups in this class.</p>
         <?php } ?>
-        <!-- Plus button to create a new group with a tooltip -->
-        <button class="add-group-button" id="createGroupButton" title="Add Group">+</button>
+         <!-- Plus button to create a new group with a tooltip -->
+         <button class="add-group-button" id="createGroupButton" title="Add Group">+</button>
     </ul>
 </body>
+
 
 <script>
     // JavaScript to handle the create group button click
